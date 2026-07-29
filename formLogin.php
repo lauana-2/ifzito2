a84d6d583bf4db1f8d05915b722b4267a2606693
lauana-2 <20241tbor0020018@estudantes.ifpr.edu.br>
2026-07-29 20:21:58 +0000

    a

---
+++ formLogin.php
@@ -0,0 +1,44 @@
+<?php include "header.php" ?>
+
+    <?php
+        //Verifica se há alguma passagem de parâmetro via método GET chamada 'erroLogin'
+        if(isset($_GET['erroLogin'])){
+            $erroLogin = $_GET['erroLogin'];
+
+            if($erroLogin == 'dadosInvalidos'){
+                echo "<div class='alert alert-warning text-center'>EMAIL ou SENHA inválidos!</div>";
+            }
+        }
+    ?>
+
+    <div class="d-flex justify-content-center">
+        <h2>Acessar o sistema:</h2>
+    </div>
+
+    <div class="d-flex justify-content-center">
+        <form action="actionLogin.php" method="POST" class="was-validated">
+            
+            <div class="form-floating mt-3 mb-3">
+                <input type="email" name="emailUsuario" id="emailUsuario" placeholder="Email" class="form-control" required>
+                <label for="emailUsuario">Email</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="Senha" class="form-control" minlength="3" maxlength="8" required>
+                <label for="senhaUsuario">Senha</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <button type="submit" class="btn btn-outline-dark">Login</button>
+
+        </form>
+    </div>
+
+    <div class="d-flex justify-content-center mt-3">
+        <p>Ainda não é cadastrado? <a href="formUsuario.php" title="Cadastrar-se">Clique aqui!</a>&nbsp<i class="bi bi-emoji-smile"></i></p>
+    </div>
+
+<?php include "footer.php" ?>
---
+++ formUsusario.php
@@ -0,0 +1,71 @@
+<?php include "header.php" ?>
+
+    <div class="d-flex justify-content-center">
+        <h2>Cadastre-se:</h2>
+    </div>
+
+    <div class="d-flex justify-content-center">
+        <form action="actionUsuario.php" method="POST" class="was-validated" enctype="multipart/form-data">
+            
+            <div class="form-floating mt-3 mb-3">
+                <input type="file" name="fotoUsuario" id="fotoUsuario" placeholder="Foto" class="form-control">
+                <label for="fotoUsuario">Foto</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome Completo" class="form-control">
+                <label for="nomeUsuario">Nome</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="date" name="dataNascimentoUsuario" id="dataNascimentoUsuario" placeholder="Data de Nascimento" class="form-control">
+                <label for="dataNascimentoUsuario">Data de Nascimento</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <select name="cidadeUsuario" id="cidadeUsuario" placeholder="Cidade" class="form-control">
+                    <option value="Curiúva">Curiúva</option>
+                    <option value="Imbaú">Imbaú</option>
+                    <option value="Ortigueira">Ortigueira</option>
+                    <option value="Reserva">Reserva</option>
+                    <option value="Telêmaco Borba" selected>Telêmaco Borba</option>
+                    <option value="Tibagi">Tibagi</option>
+                </select>
+                <label for="cidadeUsuario">Cidade</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="email" name="emailUsuario" id="emailUsuario" placeholder="Email" class="form-control">
+                <label for="emailUsuario">Email</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="Senha" class="form-control" minlength="3" maxlength="8">
+                <label for="senhaUsuario">Senha</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="password" name="confirmarSenhaUsuario" id="confirmarSenhaUsuario" placeholder="Confirmar Senha" class="form-control" minlength="3" maxlength="8">
+                <label for="confirmarSenhaUsuario">Confirmar Senha</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <button type="submit" class="btn btn-outline-dark">Cadastrar</button>
+
+        </form>
+    </div>
+
+<?php include "footer.php" ?>
